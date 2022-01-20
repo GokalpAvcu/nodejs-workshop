6efe42111080c7a261e616316999d343772f35c6
gökalp <avcu219@gmail.com>
2022-01-20 17:58:45 +0300

    add js, json

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http'); // node.js icerisinde bi http paketimiz var ve o http paketini import ettik require kullanarak
 
-const host = '127.0.0.1'; // daha sonra host'umuzun calisacagi adresi belirliyoruz
-const port = 3000; // bilgisayarımıza hangi port'tan baglanilacagini belirttik
+const host = '127.0.0.1'; // daha sonra host'umuzun calisacagi adresi belirliyoruz.
+const port = 3000; // bilgisayarımıza hangi port'tan baglanilacagini belirttik.
 
 const server = http.createServer((req, res) => { // request(kullanıcının yaptığı istek, yani ordan gelen bilgiler) ve response(bizim kullanıcıya göndereceğimiz cevaplar)
       res.statusCode = 200; //200 ile başlayan kodlar başarılı yani yapılan istek başarılı cevap verildi anlamına gelir
       res.setHeader('Content-Type', 'text/plain'); // Gönderdiğimiz cevabın başlığını belirtiyoruz
-      res.end('Gokalp Avcu Kanalina Hosgeldiniz');  // server'ı olusturmak icin http metodundan createServer isimli  bir fonksiyonu alıyoruz. // bu createServer fonksiyonu 1 adet callback fonksiyon alır ve bu aldigi callback fonksiyonun 2 adet parametresi vardır bunlar request(kullanıcının yaptığı istek, yani ordan gelen bilgiler) ve response(bizim kullanıcıya göndereceğimiz cevaplar)
+      res.end('Gokalp Avcu Kanalina Hosgeldinizz');  // server'ı olusturmak icin http metodundan createServer isimli  bir fonksiyonu alıyoruz. // bu createServer fonksiyonu 1 adet callback fonksiyon alır ve bu aldigi callback fonksiyonun 2 adet parametresi vardır bunlar request(kullanıcının yaptığı istek, yani ordan gelen bilgiler) ve response(bizim kullanıcıya göndereceğimiz cevaplar)
 
 });
 
